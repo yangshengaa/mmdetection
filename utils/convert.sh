5f44c77652025c89048231f22832f94da3d03510
yangshengaa <yangshengaa@hotmail.com>
2023-05-06 12:29:16 -0400

    added deepfashion2_to_coco and running shell script

---
+++ utils/convert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -c 4               
+#SBATCH -t 1-00:00           
+#SBATCH -p kempner 
+#SBATCH --mem=16000  
+#SBATCH -o convert_%j.out   
+#SBATCH -e convert_%j.err 
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=shengyang@fas.harvard.edu
+
+# specify path
+path="/n/pehlevan_lab/Everyone/deepfashion2/raw/test"
+
+# run convertion
+python deepfashion2_to_coco.py --path $path
